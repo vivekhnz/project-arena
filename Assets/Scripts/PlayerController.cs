6bc1d3dd95e98edb747f5bd991d00b4625a19bc1
Vivek Hari <vivek.hari@outlook.co.nz>
2016-05-15 18:12:04 +1000

    Added PlayerController script. Added basic movement.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MovementSpeed = 0.3f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float xTranslate = Input.GetAxis("Horizontal") * MovementSpeed;
+        float yTranslate = Input.GetAxis("Vertical") * MovementSpeed;
+
+        transform.Translate(xTranslate, yTranslate, 0.0f);
+    }
+}
