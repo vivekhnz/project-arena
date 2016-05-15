3d38a2a112408baeded5385e2a5ee45a4188dd57
Vivek Hari <vivek.hari@outlook.co.nz>
2016-05-15 19:36:43 +1000

    Added basic camera follow behaviour.

---
+++ Assets/Scripts/FollowObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowObject : MonoBehaviour
+{
+    public Transform target;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            // set the X and Y ordinates to the target's
+            transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        }
+    }
+}
