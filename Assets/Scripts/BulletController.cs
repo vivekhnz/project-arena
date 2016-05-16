adb8f0b7ce5ae9fc4d99e2775a7c460aee0786c4
Vivek Hari <vivek.hari@outlook.co.nz>
2016-05-16 15:26:42 +1000

    Added logic to destroy bullets once they leave the world bounds.

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -6,14 +6,30 @@ public class BulletController : MonoBehaviour
 {
     public float Speed = 1.0f;
 
+    private Rect worldBounds;
+    private Bounds spriteBounds;
+
     void Start()
     {
+        // retrieve world bounds from camera controller
+        var cameraController = Camera.main.GetComponent<CameraController>();
+        worldBounds = cameraController.Bounds;
 
+        // retrieve sprite bounds
+        var renderer = GetComponent<SpriteRenderer>();
+        spriteBounds = renderer.sprite.bounds;
     }
 
     void Update()
     {
         transform.Translate(Vector3.right * Speed);
+        if (spriteBounds.min.x + transform.position.x > worldBounds.xMax
+            || spriteBounds.max.x + transform.position.x < worldBounds.xMin
+            || spriteBounds.min.y + transform.position.y > worldBounds.yMax
+            || spriteBounds.max.y + transform.position.y < worldBounds.yMin)
+        {
+            Destroy(gameObject);
+        }
     }
 
     public void Initialize(Vector3 position, Quaternion rotation)
