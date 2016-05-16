711d20c62070c885f8bf554b13b24d1350e8f3f3
Vivek Hari <vivek.hari@outlook.co.nz>
2016-05-17 04:03:43 +1000

    Added logic to destroy Shield Generator after it takes a certain amount of damage. Sprite is tinted to red based on current health.

---
+++ Assets/Scripts/ShieldGeneratorController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldGeneratorController : PooledObject
+{
+    public int MaxHealth = 100;
+
+    private int currentHealth = 100;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        currentHealth = MaxHealth;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            currentHealth--;
+            if (currentHealth <= 0)
+            {
+                Recycle();
+            }
+            else
+            {
+                spriteRenderer.color = Color.Lerp(Color.red, Color.white, currentHealth / (float)MaxHealth);
+            }
+        }
+    }
+}
