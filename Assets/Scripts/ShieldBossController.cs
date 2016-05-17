7a78d8955285b545d457843491b6b4d4a5c5dc51
Vivek Hari <vivek.hari@outlook.co.nz>
2016-05-17 23:01:43 +1000

    Added damageable object component to boss. Added damaged animation to boss.

--- Assets/Scripts/ShieldBossController.cs
+++ Assets/Scripts/ShieldBossController.cs
@@ -6,18 +6,29 @@ public class ShieldBossController : MonoBehaviour
     public float TurnSpeed = 0.1f;
 
     private GameObject player;
+    private DamageableObject damageComponent;
+    private Animator animator;
 
     void Start ()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        animator = GetComponent<Animator>();
+        damageComponent = GetComponent<DamageableObject>();
+        if (damageComponent != null)
+        {
+            // subscribe to the health changed event
+            damageComponent.HealthChanged += OnHealthChanged;
+        }
     }
 
-	void FixedUpdate ()
+    void FixedUpdate ()
     {
         if (player != null)
         {
-            Vector2 direction = player.transform.position - transform.position;
-            var hit = Physics2D.Raycast(transform.position, direction);
+            Vector2 direction;
+            var hit = RaycastLineOfSight(out direction);
+            
+            // if we have line of sight with the player, rotate to face the player
             if (hit.collider.gameObject.tag == "Player")
             {
                 RotateTo(Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
@@ -25,12 +36,14 @@ public class ShieldBossController : MonoBehaviour
         }
     }
 
-    void OnDrawGizmosSelected()
+    void OnDrawGizmos()
     {
+        // draw line of sight in the Unity editor
         if (player != null)
         {
-            Vector2 direction = player.transform.position - transform.position;
-            var hit = Physics2D.Raycast(transform.position, direction);
+            Vector2 direction;
+            var hit = RaycastLineOfSight(out direction);
+
             if (hit.collider.gameObject.tag == "Player")
             {
                 Gizmos.color = Color.red;
@@ -46,7 +59,22 @@ public class ShieldBossController : MonoBehaviour
 
     void RotateTo(float targetRotation)
     {
+        // interpolate towards the target rotation
         Quaternion target = Quaternion.Euler(0.0f, 0.0f, targetRotation);
         transform.rotation = Quaternion.Lerp(transform.rotation, target, TurnSpeed);
+    }
+
+    private RaycastHit2D RaycastLineOfSight(out Vector2 direction)
+    {
+        direction = player.transform.position - transform.position;
+        // only raycast against objects on the Player (9) and Terrain (10) layers
+        int layerMask = (1 << 9) | (1 << 10);
+        return Physics2D.Raycast(transform.position, direction, float.MaxValue, layerMask);
+    }
+
+    private void OnHealthChanged(object sender, System.EventArgs e)
+    {
+        // play the damaged animation
+        animator.SetTrigger("OnDamaged");
     }
 }
