7e8d02db9d1624df67edf4091bc21b8f333590b8
Vivek Hari <vivek.hari@outlook.co.nz>
2016-05-17 20:44:10 +1000

    Added logic to make boss face the player.

---
+++ Assets/Scripts/ShieldBossController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldBossController : MonoBehaviour
+{
+    private GameObject player;
+
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void FixedUpdate ()
+    {
+        Vector3 direction = player.transform.position - transform.position;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f,
+            Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+    }
+}
