02cb6f58e4afe814add7349b23893ad8356cd024
Vivek Hari <vivek.hari@outlook.co.nz>
2016-05-17 21:37:26 +1000

    Added logic so boss only faces player if it has line of sight.

--- Assets/Scripts/ShieldBossController.cs
+++ Assets/Scripts/ShieldBossController.cs
@@ -3,17 +3,50 @@ using System.Collections;
 
 public class ShieldBossController : MonoBehaviour
 {
+    public float TurnSpeed = 0.1f;
+
     private GameObject player;
 
-	void Start ()
+    void Start ()
     {
         player = GameObject.FindGameObjectWithTag("Player");
-	}
+    }
 
 	void FixedUpdate ()
     {
-        Vector3 direction = player.transform.position - transform.position;
-        transform.rotation = Quaternion.Euler(0.0f, 0.0f,
-            Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+        if (player != null)
+        {
+            Vector2 direction = player.transform.position - transform.position;
+            var hit = Physics2D.Raycast(transform.position, direction);
+            if (hit.collider.gameObject.tag == "Player")
+            {
+                RotateTo(Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (player != null)
+        {
+            Vector2 direction = player.transform.position - transform.position;
+            var hit = Physics2D.Raycast(transform.position, direction);
+            if (hit.collider.gameObject.tag == "Player")
+            {
+                Gizmos.color = Color.red;
+            }
+            else
+            {
+                Gizmos.color = Color.white;
+            }
+            
+            Gizmos.DrawRay(transform.position, direction.normalized * hit.distance);
+        }
+    }
+
+    void RotateTo(float targetRotation)
+    {
+        Quaternion target = Quaternion.Euler(0.0f, 0.0f, targetRotation);
+        transform.rotation = Quaternion.Lerp(transform.rotation, target, TurnSpeed);
     }
 }
