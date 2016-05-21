dcf02b80d2060a594dff611df17fb8269d253893
Vivek Hari <vivek.hari@outlook.co.nz>
2016-05-21 19:49:32 +1000

    Added Seeker Line Squad prefab. Added logic to allow Seeker enemies to follow a non-player object e.g. another Seeker.

--- Assets/Scripts/SeekerEnemyController.cs
+++ Assets/Scripts/SeekerEnemyController.cs
@@ -3,39 +3,39 @@ using System.Collections;
 
 public class SeekerEnemyController : MonoBehaviour
 {
-    public float InitialTurnSpeed = 0.05f;
-    public float FinalTurnSpeed = 0.1f;
-    public float TurnSpeedTransitionDurationSeconds = 5.0f;
+    public float TurnSpeed = 0.05f;
     public float MovementSpeed = 0.5f;
+    public GameObject FollowTarget;
 
     private GameObject player;
     private float startTime;
 
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
         startTime = Time.time;
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     void FixedUpdate()
     {
-        if (player != null)
+        if (FollowTarget == null)
+        {
+            FollowTarget = GameObject.FindGameObjectWithTag("Player");
+        }
+        GameObject target = FollowTarget ?? player;
+        if (target != null)
         {
             // rotate to face the player
-            Vector2 direction = player.transform.position - transform.position;
-            RotateTo(Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+            Vector2 direction = target.transform.position - transform.position;
+            RotateTo(Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg, TurnSpeed);
         }
 
         // move the enemy in the direction it is facing
         transform.Translate(Vector3.right * MovementSpeed);
     }
 
-    void RotateTo(float targetRotation)
+    void RotateTo(float targetRotation, float turnSpeed)
     {
-        // calculate current turn speed
-        float turnSpeedProgress = Mathf.Min((Time.time - startTime) / TurnSpeedTransitionDurationSeconds, 1.0f);
-        float turnSpeed = Mathf.Lerp(InitialTurnSpeed, FinalTurnSpeed, turnSpeedProgress);
-
         // interpolate towards the target rotation
         Quaternion target = Quaternion.Euler(0.0f, 0.0f, targetRotation);
         transform.rotation = Quaternion.Lerp(transform.rotation, target, turnSpeed);
