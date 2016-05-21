b46d6f0b86cf9bec8550c82148ec4bb295fdaa09
Vivek Hari <vivek.hari@outlook.co.nz>
2016-05-22 02:43:56 +1000

    Added parallax scrolling to grid background.

---
+++ Assets/Scripts/ParallaxScrolling.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxScrolling : MonoBehaviour
+{
+    public Transform Camera;
+    public float ParallaxMultiplier;
+
+    private Vector3 offset;
+
+	void Start ()
+    {
+        offset = transform.position;
+	}
+	
+	void Update ()
+    {
+        transform.position = new Vector3(
+            offset.x + (Camera.transform.position.x * ParallaxMultiplier),
+            offset.y + (Camera.transform.position.y * ParallaxMultiplier),
+            offset.z);
+	}
+}
