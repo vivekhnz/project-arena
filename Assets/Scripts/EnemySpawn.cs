3cf2f7f7ab3710e4bc7914136bc396167d27d005
Vivek Hari <vivek.hari@outlook.co.nz>
2016-05-25 16:14:49 +1000

    Added support for spawning different types of enemies.

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+[Serializable]
+public class EnemySpawn
+{
+    public EnemyController Enemy;
+    public int EnemiesPerSpawn = 5;
+}
