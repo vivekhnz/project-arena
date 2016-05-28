dd1e581ee97d972f1d09b63d8b4b54771cdcdb84
Vivek Hari <vivek.hari@outlook.co.nz>
2016-05-28 22:24:37 +1000

    Added basic particle system.

---
+++ Assets/Scripts/ParticleSystemSortingLayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleSystemSortingLayer : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+        ParticleSystem particleSystem = GetComponent<ParticleSystem>();
+        Renderer renderer = particleSystem.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.sortingLayerName = "Particles";
+            renderer.sortingOrder = 2;
+        };
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
