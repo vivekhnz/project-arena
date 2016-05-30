80b0106512497b90f5bfa912aa371c17858e4561
Vivek Hari <vivek.hari@outlook.co.nz>
2016-05-30 18:15:38 +1000

    Added player animations. Added basic super meter.

---
+++ Assets/Scripts/SuperMeterController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SuperMeterController : MonoBehaviour
+{
+    public PlayerController Player;
+
+	void Start ()
+    {
+
+	}
+	
+	void FixedUpdate ()
+    {
+        transform.position = Player.transform.position;
+	}
+}
