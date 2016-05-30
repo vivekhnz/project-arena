5f64d44f53b598448da386a7badae97c5ac316e3
Vivek Hari <vivek.hari@outlook.co.nz>
2016-05-30 19:30:38 +1000

    Added animations for super meter charged state.

---
+++ Assets/Scripts/WorldUIController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WorldUIController : MonoBehaviour
+{
+    public PlayerController Player;
+    public GameObject SuperMeter;
+    public Image SuperMeterForeground;
+
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        SuperMeter.transform.position = Player.transform.position;
+        SuperMeterForeground.fillAmount = Mathf.Lerp(
+            SuperMeterForeground.fillAmount,
+            Player.SuperEnergy, 0.25f);
+
+        if (animator != null)
+        {
+            animator.SetFloat("SuperEnergy", Player.SuperEnergy);
+        }
+    }
+}
