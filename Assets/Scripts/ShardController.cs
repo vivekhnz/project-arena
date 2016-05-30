6d56b9ccd57bab88148ed7e85be2a63514b0ec44
Vivek Hari <vivek.hari@outlook.co.nz>
2016-05-31 04:20:40 +1000

    Added logic to recycle shard when it collides with the Player.

---
+++ Assets/Scripts/ShardController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShardController : PooledObject
+{
+    private Rect worldBounds;
+    private Bounds spriteBounds;
+    private ExplosionManager explosionManager;
+
+    void Start()
+    {
+        // retrieve world bounds from camera controller
+        var cameraController = Camera.main.GetComponent<CameraController>();
+        worldBounds = cameraController.Bounds;
+
+        // retrieve sprite bounds
+        var renderer = GetComponent<SpriteRenderer>();
+        spriteBounds = renderer.sprite.bounds;
+    }
+
+    void FixedUpdate()
+    {
+        // recycle the object if it leaves the world bounds
+        if (spriteBounds.min.x + transform.position.x > worldBounds.xMax
+            || spriteBounds.max.x + transform.position.x < worldBounds.xMin
+            || spriteBounds.min.y + transform.position.y > worldBounds.yMax
+            || spriteBounds.max.y + transform.position.y < worldBounds.yMin)
+        {
+            Recycle();
+        }
+    }
+
+    public void Initialize(Vector3 position)
+    {
+        transform.position = position;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        ManageCollisions(collision);
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        ManageCollisions(collision);
+    }
+
+    private void ManageCollisions(Collision2D collision)
+    {
+        string[] tags = collision.gameObject.tag.Split('|');
+        foreach (string tag in tags)
+        {
+            switch (tag)
+            {
+                case "Player":
+                    Recycle();
+                    break;
+            }
+        }
+    }
+}
