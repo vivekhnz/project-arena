9bfc7d02342dcbe0abdcf11c43fb44aaaa115e97
Vivek Hari <vivek.hari@outlook.co.nz>
2016-06-02 18:47:18 +1000

    Added shockwave effect.

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -2,16 +2,18 @@
 using System.Collections;
 using System.Linq;
 
-public class AudioIntensityController : MonoBehaviour
+public class AudioController : MonoBehaviour
 {
     public SpriteRenderer Background;
 
     AudioSource audioSource;
+    AudioLowPassFilter lowpass;
     float[] spectrum = new float[256];
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        lowpass = GetComponent<AudioLowPassFilter>();
     }
 
     void Update()
@@ -31,5 +33,19 @@ public class AudioIntensityController : MonoBehaviour
             Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, 25.0f, 0.05f);
             Background.color = Color.Lerp(Background.color, new Color(0.1f, 0.1f, 0.1f), 0.1f);
         }
+
+        if (lowpass.cutoffFrequency < 5000)
+        {
+            lowpass.cutoffFrequency = Mathf.Lerp(lowpass.cutoffFrequency, 5000, 0.02f);
+            if (lowpass.cutoffFrequency > 4995)
+            {
+                lowpass.cutoffFrequency = 5000;
+            }
+        }
+    }
+
+    public void ApplyShockwaveEffect()
+    {
+        lowpass.cutoffFrequency = 50;
     }
 }
--- Assets/Scripts/ShockwaveController.cs
+++ Assets/Scripts/ShockwaveController.cs
@@ -6,5 +6,12 @@ public class ShockwaveController : PooledObject
     public void Initialize(Vector3 position)
     {
         transform.position = position;
+        
+        var audioControllerObj = GameObject.FindGameObjectWithTag("AudioController");
+        if (audioControllerObj != null)
+        {
+            var audioController = audioControllerObj.GetComponent<AudioController>();
+            audioController.ApplyShockwaveEffect();
+        }
     }
 }
