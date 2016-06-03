74f494e1b6b47d37e03e1bee0ac9e280736cfb49
Vivek Hari <vivek.hari@outlook.co.nz>
2016-06-03 16:00:28 +1000

    Added Swarm enemy controller. Adjusted enemy spawns in wave 1 to include Swarm enemy.

---
+++ Assets/Scripts/SwarmEnemyController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(EnemyController))]
+public class SwarmEnemyController : MonoBehaviour
+{
+    private EnemyController enemyController;
+    private bool initialized = false;
+
+    void Start()
+    {
+        enemyController = GetComponent<EnemyController>();
+        enemyController.InstanceReset += OnInstanceReset;
+
+        initialized = false;
+    }
+
+    private void OnInstanceReset(object sender, System.EventArgs e)
+    {
+        initialized = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (!initialized)
+        {
+            Vector2 directionToCenter = -transform.position.normalized;
+            float angle = Mathf.Atan2(directionToCenter.y, directionToCenter.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0.0f, 0.0f, angle);
+
+            initialized = true;
+        }
+        if (!enemyController.IsEscaping)
+        {
+            transform.Translate(Vector3.right * enemyController.MovementSpeed);
+        }
+    }
+}
