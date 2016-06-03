4703062601314dacccc5e7f3b2c414e911e5a803
Vivek Hari <vivek.hari@outlook.co.nz>
2016-06-03 22:16:52 +1000

    Added enemy spawn hazard indicators.

---
+++ Assets/Scripts/HazardController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HazardController : PooledObject
+{
+    public void Initialize(Vector3 position)
+    {
+        transform.position = position;
+    }
+}
